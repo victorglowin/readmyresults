acdcef322a44b8e7a172d7f13c31f79daac433f4
Victor Kelechi <victorglowin@gmail.com>
2026-06-20 01:06:07 -0700

    Add file upload endpoint

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 app = FastAPI()
 @app.get("/")
 def read_root():
     return{"message": "ReadMyResults backend is alive"}
+
+@app.post("/upload")
+async def upload_documant(file: UploadFile = File(...)):
+    contents = await file.read()
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "size_bytes": len(contents)
+    }
